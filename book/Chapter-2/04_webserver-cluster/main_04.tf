9d5661cdd5af69493ade32776faca79beefde31b
Vladimir Pavlychev <vladimir.keyjoo@gmail.com>
2020-10-13 20:20:12 +0300

    Fix: :recycle: prepare files for websrver-cluster

--- book/Chapter-2/04_webserver-cluster/main_04.tf
+++ book/Chapter-2/04_webserver-cluster/main_04.tf
@@ -1,8 +1,12 @@
+terraform {
+  required_version = ">= 0.13"
+}
+
 provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_instance" "configured_web_server" {
+resource "aws_instance" "cluster_web_server" {
   #Ubuntu Server 18.04 LTS (HVM), SSD Volume Type - ami-092391a11f8aa4b7b (64-bit x86)
   ami = "ami-092391a11f8aa4b7b"
   instance_type          = "t2.micro"
@@ -10,7 +14,7 @@ resource "aws_instance" "configured_web_server" {
 
   user_data = <<-EOF
             #!/bin/bash
-            echo "Configured_web_server runned by Vladimir Pavlychev at AWS ::EU from Protvino(Moscow,Russia)" > index.html
+            echo "Cluster_web_server runned by Vladimir Pavlychev at AWS ::EU from Protvino(Moscow,Russia)" > index.html
             nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
