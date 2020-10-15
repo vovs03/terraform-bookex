8ce55dc638c53d39524dea9577a435a14351232e
Vladimir Pavlychev <vladimir.keyjoo@gmail.com>
2020-10-15 13:10:14 +0300

    Add: :new: -launch_conf-

--- book/Chapter-2/04_webserver-cluster/README.md
+++ book/Chapter-2/04_webserver-cluster/README.md
@@ -2,3 +2,6 @@
 
 ## 2.04.1
 
+- [x] 7.1 var `"instance_security_group_name"` 
+- [x] 7.2 `AS` - #7 Конфигурация запуска
+  - `launch_conf`
--- book/Chapter-2/04_webserver-cluster/main_04.tf
+++ book/Chapter-2/04_webserver-cluster/main_04.tf
@@ -1,27 +1,45 @@
 terraform {
-  required_version = ">= 0.13"
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+  # For Terraform ~> v.0.13.4
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
 provider "aws" {
+  # Frankfurt
   region = "eu-central-1"
 }
 
-resource "aws_instance" "cluster_web_server" {
+# Конфигурация запуска
+resource "aws_launch_configuration" "launch_conf" {
   #Ubuntu Server 18.04 LTS (HVM), SSD Volume Type - ami-092391a11f8aa4b7b (64-bit x86)
-  ami = "ami-092391a11f8aa4b7b"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  image_id        = "ami-092391a11f8aa4b7b"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
             #!/bin/bash
             echo "Cluster_web_server runned by Vladimir Pavlychev at AWS ::EU from Protvino(Moscow,Russia)" > index.html
             nohup busybox httpd -f -p ${var.server_port} &
               EOF
+  
+  # Требуетсяпри использовании автоскейлинг гроуп
+  # в конфигурации запуска
+  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags = {
-    Name = "Terraform-configured-web"
+    #Name = "Terraform-"
   }
 }
+
+resource ""
 
 resource "aws_security_group" "instance"{
   name = "terraform-example-instance"
