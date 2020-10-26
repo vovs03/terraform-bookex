97ec2f9be85c4cb0c1fec3fb13268127239a1468
Vladimir Pavlychev <vladimir.keyjoo@gmail.com>
2020-10-26 09:30:18 +0300

    Add: :new: aws_lb_listener - default 404 page

--- book/Chapter-2/04_webserver-cluster/main_04.tf
+++ book/Chapter-2/04_webserver-cluster/main_04.tf
@@ -74,3 +74,20 @@ resource "aws_lb" "example" {
   load_balancer_type = "application"
   subnets = data.aws_subnet_ids.default.ids
 }
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
