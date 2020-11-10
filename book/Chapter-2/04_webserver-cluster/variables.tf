78f8cb4a97092291f9a6a938ccb6162bbd9cbe0d
Vladimir Pavlychev <vladimir.keyjoo@gmail.com>
2020-11-10 11:31:12 +0300

    Add: 3.1 alb_security_group_name - #8

--- book/Chapter-2/04_webserver-cluster/variables.tf
+++ book/Chapter-2/04_webserver-cluster/variables.tf
@@ -9,3 +9,10 @@ variable "instance_security_group_name" {
   type        = string
   default     = "terraform-example-instance"
 }
+
+# Step 04.03 
+variable "alb_security_group_name" {
+  description = "The name of the security group for the ALB"
+  type        = string
+  default     = "terraform-example-alb"
+}
